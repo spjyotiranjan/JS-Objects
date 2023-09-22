9ac8b9fde24a1529df76c03e834ddc1e8d0a04e6
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-09-22 07:20:46 +0530

    Done

--- script.js
+++ script.js
@@ -5,18 +5,35 @@
 // cohortName,
 // location,
 //hobbies.
+var studentProfile = {
+    studentName : "SP Jyotiranjan Sahoo",
+    studentAge : 18,
+    cohortName : "Squad 54",
+    location : "Punjab",
+    hobbies : "GYM"
+}
 
 // Step 2: Add more then one hobby in the student object.
 //Hint: Use Arrays
 
 // Your code goes here
+var student = {
+    hobbies : ["Freelancing","Cricket","GYM"]
+}
 
 // Challenge 1.2: Access the studentName value of the object "studentProfile" using dot "." notation and print the same in console.
+console.log(studentProfile.studentName);
 
 //Challenge 1.3: Access the cohortName property of the object using Bracket notation ([]).
+console.log(studentProfile["cohortName"]);
 
 //Challenge 2.1:Create an object using the constructor function for storing the book information. The object name can be "Book". Include the following values .
 //title
 //author
-
+function Book(title,author) {
+    this.title = title;
+    this.author = author;
+}
 //Challenge 2.2: Access the title property of the object "Book" and print the same in console.
+Book.title = "Ikagai"
+console.log(Book.title);
